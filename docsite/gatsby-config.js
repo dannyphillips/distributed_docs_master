3ee7379c86a7ba876b64d7bb2fdfa42b5df36c8f
Danny Phillips <danny.phillips8@gmail.com>
2020-05-21 22:32:51 -0700

    setup markdown loading

--- docsite/gatsby-config.js
+++ docsite/gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-docsite`, options: {} }],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+    `gatsby-theme-docsite`,
+  ],
+};
