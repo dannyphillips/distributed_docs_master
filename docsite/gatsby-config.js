2cda9464f236c1d82fdbd73ff710a3e55f3086ed
Danny Phillips <danny.phillips8@gmail.com>
2020-05-22 10:07:00 -0700

    install gatsby-source-git for loading markdown from other repos

--- docsite/gatsby-config.js
+++ docsite/gatsby-config.js
@@ -13,6 +13,22 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: `repo-one`,
+        remote: `https://github.com/dannyphillips/distributed_docs_service_1.git`,
+        patterns: [`*`, `!*.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: `repo-two`,
+        remote: `https://github.com/dannyphillips/distributed_docs_service_2.git`,
+        patterns: [`*`, `!*.md`],
+      },
+    },
     `gatsby-plugin-mdx`,
     `gatsby-theme-docsite`,
   ],
